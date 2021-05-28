797845f4a156944da21da34c330670b8694eedc3
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-28 23:51:49 +0300

    function for validation

--- user_Class.py
+++ user_Class.py
@@ -2,7 +2,7 @@ class User:
     '''
     Class that creates instances of user details.
     '''
-    user_data =[]
+    user_data = []
 
     def __init__(self, username, password):
         self.username = username
@@ -14,7 +14,14 @@ class User:
         '''
 
         User.user_data.append(self)
-        
 
+    @classmethod
+    def user_exist(cls, username, password):
+        '''
+        Method that aunthenticates username and password
+        '''
+        for user in cls.user_data:
+            if (user.username == username and user.password == password):
+                return True
 
-
+        return False
