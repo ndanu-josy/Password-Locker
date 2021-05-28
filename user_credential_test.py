14a74e2c3bfdf7aa4f9372cb593c0092be626085
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-28 22:33:47 +0300

    Test to save contact

--- user_credential_test.py
+++ user_credential_test.py
@@ -2,27 +2,36 @@ import unittest # Importing the unittest module
 from user_Class import User # Importing the user class
 
 class TestUser(unittest.TestCase):
-     '''
-     Test class that defines test cases for the user class 
+    '''
+    Test class that defines test cases for the user class 
 
-     Args: 
-     unittest.TestCase: helps in creating test cases
-     '''   
-     def setUp(self):
+    Args: 
+    unittest.TestCase: helps in creating test cases
+    '''   
+    def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
         self.new_user = User("ndanu-josy", "2021") # create user object
 
 
-     def test_init(self):
+    def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
 
         self.assertEqual(self.new_user.username,"ndanu-josy")
-        self.assertEqual(self.new_user.password,"2021")        
+        self.assertEqual(self.new_user.password,"2021")  
 
+    def test_save_user(self):
+        '''
+        test case to test if the user object is saved
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_data),1)
+      
+
+    
 
 if __name__ == '__main__':
     unittest.main()     
