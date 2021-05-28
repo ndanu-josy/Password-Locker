8b33b7dee02cb2b599709cf49679c2d9aa0383a3
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-28 22:00:06 +0300

    Importing user class

--- user_credential_test.py
+++ user_credential_test.py
@@ -1,21 +1,21 @@
 import unittest # Importing the unittest module
-from user_Class import User # Importing the contact class
+from user_Class import User # Importing the user class
 
 class TestUser(unittest.TestCase):
-    '''
-    Test class that defines test cases for the user class 
+     '''
+     Test class that defines test cases for the user class 
 
-    Args: 
-        unittest.TestCase: helps in creating test cases
+     Args: 
+     unittest.TestCase: helps in creating test cases
      '''   
-    def setUp(self):
+     def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
         self.new_user = User("ndanu-josy", "2021") # create user object
 
 
-    def test_init(self):
+     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
