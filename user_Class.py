e532a0125b3e661e0066a4d5cc07c72a7e29d57e
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-28 21:30:45 +0300

    User class

---
+++ user_Class.py
@@ -0,0 +1,11 @@
+class User:
+    '''
+    Class that creates instances of user details.
+    '''
+    user_data =[]
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
