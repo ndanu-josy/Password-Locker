d899fcf77cef3b1ee051c748ce5514ce513013da
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-29 12:13:39 +0300

    constructor

---
+++ credentials_class.py
@@ -0,0 +1,15 @@
+class Credentials:
+    """
+    Class that generates new instances of user credentials
+    """
+    def __init__(self, social_media, username, password):
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        '''
+        self.social_media = social_media
+        self.username = username
+        self.password = password
+
+
+
