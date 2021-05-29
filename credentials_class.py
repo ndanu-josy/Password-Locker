8e4174047df465ca48ba24035d08eaf7ad40b06c
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-29 12:19:20 +0300

    Empty list to store credentials

--- credentials_class.py
+++ credentials_class.py
@@ -1,13 +1,17 @@
-class Credentials:
+class Credentials:    
     """
     Class that generates new instances of user credentials
     """
-    def __init__(self, social_media, username, password):
+
+    credentials_list = []
+
+    def __init__(self, account_name, username, password):
+        
         '''
         __init__ method that helps us define properties for our objects.
 
         '''
-        self.social_media = social_media
+        self.account_name = account_name
         self.username = username
         self.password = password
 
