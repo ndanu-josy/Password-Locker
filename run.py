b7bbb3bf2db55bf02129e3825b124037d524f1a0
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-29 18:01:41 +0300

    login and create account short codes

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3.8
 from user_class import User  # Importing the user class
 from credentials_class import Credentials
-
+    
+    #user
 def create_user(username, password):
     '''
     Funcion to create a new user
@@ -56,43 +57,67 @@ def display_credentials():
     '''
 
 
-
-
-
 # main
 def main():
     print("Hello, Welcome to PassworLocker. Create an account to save your passwords")
-    print('Enter your username')
-    username = input()
-    print("What's your password")
-    password = input()
-    print("Confirm password ....")
-    confirm_password = input()
-    
-    while confirm_password != password:
-                print("Password did not match")
-                print("Enter password again")
-                password = input()
-                print("Confirm password ....")
-                confirm_password = input()
-    else:
-                save_user(create_user(username, password))
-                print(f'Congratulations 🎉, New Account has been created for: {username} using password: {password}')
-                print("proceed to login")
-                print("username")
-                entered_username = input()
-                print("your password")
-                entered_password = input()
+    print("...............")
+    print('\n')
 
-    if          entered_username!= username or entered_password != password:
-                print("Invalid username or password")
-                print('first name')
-                entered_username= input()
-                print("Your password")
-                entered_password = input()
-                 
+    while True:
+        print("Choose a shortcode to proceed : ca - create an account, ex - to exit")
+        short_code = input('Enter a choice: ').lower()
+
+        if short_code == 'ex':
+            print("Goodbye")
+            break
+        elif short_code == 'ca':
+
+            print('Enter your username')
+            username = input()
+            print("What's your password")
+            password = input()
+            print("Confirm password ....")
+            confirm_password = input()
         
-    print(f'Welcome back  {entered_username} 😍. please choose an option to continue')
+            while confirm_password != password:
+                    print("Password did not match")
+                    print("Enter password again")
+                    password = input()
+                    print("Confirm password ....")
+                    confirm_password = input()
+            else:
+                    save_user(create_user(username, password))
+                    print(f'Congratulations, New Account has been created for: {username} using password: {password}')
+                    print("Choose a shortcode to proceed : lg - login, ex-exit")
+                    short_code = input('Enter a choice: ').lower()
+
+                    if short_code == 'ex':
+                        print("Goodbye")
+                        break
+                    elif short_code == 'lg':
+                 
+                            print("proceed to login")
+                            print("username")
+                            entered_username = input()
+                            print("your password")
+                            entered_password = input()
+
+                    if  entered_username!= username or entered_password != password:
+                                print("Invalid username or password")
+                                print('first name')
+                                entered_username= input()
+                                print("Your password")
+                                entered_password = input()                        
+                            
+                    
+        
+        break
+
+    while True:
+        print('\n')
+        print(f'Welcome back  {entered_username}')
+        print('Use these codes to navigate: nc -create a credential, dc-dipslay credentials, cp-copy credential, ex-exit')
+        short_code = input('Enter your choice: ').lower()
 
 if __name__ == '__main__':
     main()    
